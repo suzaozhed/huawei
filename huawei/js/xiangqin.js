d935d0ff3b5b109a063f7975495c2f111a95f713
suzaozhed <1487820857@qq.com>
2018-08-07 21:23:37 +0800

    8/7/2

--- huawei/js/xiangqin.js
+++ huawei/js/xiangqin.js
@@ -1,2 +1,15 @@
-var btn = document.querySelectorAll("dda_left_buttom_ul_li")
-console.log(btn)
+var btn = document.getElementsByClassName("dda_left_buttom_ul_li");
+var box = document.getElementsByClassName("dda_left_top_li");
+for(var i = 0;i<btn.length;i++){
+    btn[i].style.border = "none";
+    btn[i].onmouseover = function(){
+        this.style.border = "1px solid #ca141d";
+        return btn[i].onmouseout = function(){
+            btn[i].style.border = "none";
+        }
+    }
+    
+    btn[i].onclick = function(){
+        box[i].style.z-index == "999"
+    }
+}
